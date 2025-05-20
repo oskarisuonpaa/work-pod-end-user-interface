fbc862ae0e2dfae6fac9d2a86658e1d83b04a349
eleino <leinonemmi@gmail.com>
2025-05-20 14:49:30 +0300

    Added some text to info page
    Added some text to info page.

--- app/src/components/Info/index.tsx
+++ app/src/components/Info/index.tsx
@@ -3,19 +3,14 @@ import "./Info.css";
 
 const Info = () => {
 return (
-    <>
-<h2>Info - Workpod Reservation System</h2>
+<div id="info">
+<h1>Info - Workpod Reservation System</h1>
 <p>Note: access is for authorized users only; log in using LAB/LUT credentials.</p>
-<p>To reserve a work pod, select a work pod from the workpods page, or search for
-    an available work pod using the search page.</p>
-    <p> You can also view your reservations on the dashboard page.</p>
-<p>To cancel a reservation, go to the dashboard page and select the reservation you
-    want to cancel.</p>
-<p>Please cancel your reservation if you no longer need it. This will help other
-    users find available work pods.</p>
+<p>To reserve a work pod, select a work pod from the workpods page, or search for an available work pod using the search page. You can view your reservations on the dashboard page.</p>
+<p>To cancel a reservation, go to the dashboard page and select the reservation you want to cancel. Please cancel your reservation if you no longer need it to make it available for another user.</p>
 
-<p>If you have any questions or feedback, please contact the IT department.</p>
-</>
+</div>
+
 )};
 
 export default Info;
