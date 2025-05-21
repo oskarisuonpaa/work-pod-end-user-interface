27786a41091e699fb7dcc62c51ed7edc8bb26b4b
eleino <leinonemmi@gmail.com>
2025-05-21 13:56:29 +0300

    Added a handler for form data
    Added a handler for the form data

--- app/src/components/Search/index.tsx
+++ app/src/components/Search/index.tsx
@@ -9,27 +9,31 @@ import "react-datepicker/dist/react-datepicker.css";
 registerLocale("fi", fi);
 setDefaultLocale("fi");
 
-// for search we need calendar and time picker
-// search based on classroom, is it even necessary?
-// reservation status for right now is visible on the workpods page
-// but it could still be useful to include?
 // search is mainly needed if someone doesn't care which workpod they use
 // and just wants to find an available one
-// how to make a timepicker?
-// maybe use a library like react-time-picker or react-datetime
-// for duration, include 30 mins, 1 hour etc
-// is 8 hours too long?
-// can time be omitted?
 // what are the "working hours" for the workpods? 8-16, 6-20?
-// leave it freeform so someone can reserve at night if they really want to?
+// once search parameters are in, look through the calendars of the workpods
+// to see if any are available at the selected time
 // TODO: custom css for datepicker
+
 const Search = () => {
-      const [startDate, setStartDate] = useState<Date | null>(null);
+      const [startDate, setStartDate] = useState<Date | null>(new Date());
+        const handleSubmit = (event: React.FormEvent) => {
+            event.preventDefault();
+            const form = event.target as HTMLFormElement;
+            const duration = form.duration.value;
+            const date = startDate; // contains both date and time
+            if (date) {
+            console.log("Selected date:", date);
+            console.log("Selected duration:", duration);
+            }
+            // do something with the data
+        }
 return (
 <div id="searchContainer">
     <h1>Search for available workpods</h1>
     <p>You can make a reservation up to 30 days in advance.</p>
-    <form>
+    <form id="searchForm" onSubmit={handleSubmit}>
         <label htmlFor="date">Date:</label>
         <DatePicker
       selected={startDate}
@@ -45,14 +49,14 @@ return (
         <DatePicker
         showTimeSelect 
         selected={startDate}
-        onChange={(date) => setStartDate(date)}
+        onChange={(time) => setStartDate(time)}
         showTimeSelectOnly
         dateFormat="HH:mm"
         calendarClassName="custom-time"
         />
         <br />
         <label htmlFor="duration">Duration:</label>
-        <input type="number" id="duration" name="duration" min="0" max="8" /> hours<br />
+        <input type="number" id="duration" name="duration" min="0" max="8"/> hours<br />
 
         <button type="submit" id="searchButton">Search</button>
     </form>
