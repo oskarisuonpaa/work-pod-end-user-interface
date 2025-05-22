d9f99e6bb7347e7138e375bc49584fb163481cf4
eleino <leinonemmi@gmail.com>
2025-05-22 09:04:51 +0300

    included the JSON data format

--- app/src/components/SearchResults/index.tsx
+++ app/src/components/SearchResults/index.tsx
@@ -7,6 +7,9 @@ const SearchResults = () => {
     const { date } = location.state || {}; // use optional chaining to avoid errors if state is undefined
     console.log("SearchResults date:", date);
     // send data to the backend - may need converting to the right format
+    // after JSON.stringify data looks like this:
+    // "2025-05-22T05:56:33.257Z"
+    // it was 8.56 so the time is in GMT
     // handle the response
     // display the results
     return (
