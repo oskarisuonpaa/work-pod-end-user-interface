548fabd282d76e05dcfdfe63b04410d6daddf115
eleino <leinonemmi@gmail.com>
2025-05-22 09:48:09 +0300

    Formatting date for query to backend
    Formatting date for query to backend

--- app/src/components/SearchResults/index.tsx
+++ app/src/components/SearchResults/index.tsx
@@ -1,21 +1,30 @@
 // SearchResults
 import { useLocation } from "react-router";
+import { format } from "date-fns";
 
 const SearchResults = () => {
     // receive data from the search page
     const location = useLocation();
     const { date } = location.state || {}; // use optional chaining to avoid errors if state is undefined
     console.log("SearchResults date:", date);
-    // send data to the backend - may need converting to the right format
-    // after JSON.stringify data looks like this:
-    // "2025-05-22T05:56:33.257Z"
-    // it was 8.56 so the time is in GMT
+    // send data to the backend 
+    // query to backend in this format:
+    // /events?calendarId=C220-1&timeMin=2025-05-25T00:00:00+03:00&timeMax=2025-05-26T00:00:00+03:00
+    // so timeMin is time from date, timeMax is end of day
+    // convert date to the right format
+    const dateString = format(date, "yyyy-MM-dd'T'HH:mm+03");
+    const timeMin = dateString;
+    const timeMax = format(date, "yyyy-MM-dd'T'23:59+03");
+    const queryString = `/events?calendarId=C220-1&timeMin=${timeMin}&timeMax=${timeMax}`;
+    console.log(queryString);
+    // 2025-05-22T09:12+03
     // handle the response
+
     // display the results
     return (
         <>
         <h1>SearchResults</h1>
-        <p>SearchResults: {JSON.stringify(date)}</p>
+        <p>SearchResults: {dateString}</p>
         </>
     );
 } 
