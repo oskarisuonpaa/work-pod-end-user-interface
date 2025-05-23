d0d3e3e40b9e6d16fd1fdf35a8abe58cc9a0b6b4
eleino <leinonemmi@gmail.com>
2025-05-23 14:20:38 +0300

    Filtering workpods
    Filtering and ordering workpods

--- app/src/components/SearchResults/index.tsx
+++ app/src/components/SearchResults/index.tsx
@@ -1,6 +1,6 @@
 // SearchResults
 import { useLocation } from "react-router";
-import { minutesToHours, differenceInMinutes, format, isWithinInterval, setHours, setMinutes } from "date-fns";
+import { minutesToHours, differenceInMinutes, format, isWithinInterval, setHours, setMinutes, add } from "date-fns";
 import { useEffect, useState } from "react";
 import "./SearchResults.css";
 
@@ -17,7 +17,8 @@ const SearchResults = () => {
     const backendUrl = import.meta.env.VITE_BACKEND_URL;
     useEffect(() => {
         // fetch list of workpods
-        if (!date) return;
+        if (!date || !loading) return;
+        if (workPods.length > 0) return;
         setDateString(date.toISOString());
         fetch(backendUrl + "calendars", {
             headers: {
@@ -33,24 +34,24 @@ const SearchResults = () => {
                     pods.push({ workpodId: calendars.calendars[id], isReserved: false, freeFor: 0, events: [] });
                 }
                 setWorkPods(pods);
+
             })
             .catch(error => console.error(error));
     }, [date]);
 
 
-    console.log("workpods:", workPods);
     // fetch data for each workpod
     useEffect(() => {
-        if (workPods.length === 0 || !date) return;
+        if (workPods.length === 0 || !date || !loading) return;
+        const dateMinute = add(date, { minutes: 1 }); // interval check returns true if reservation ends at 15:00 and date is 15:00
         workPods.forEach((workpod, idx) => {
             const workpodId = workpod.workpodId;
             setDateString(date.toISOString()); //format(date, "yyyy-MM-dd'T'HH:mm+03");
             const timeMin = dateString;
             const timeMax = format(date, "yyyy-MM-dd'T'23:00:00'Z'");
             const queryString = `events?calendarId=${workpodId}&timeMin=${timeMin}&timeMax=${timeMax}`;
-            console.log(queryString);
+
             const url = backendUrl + queryString;
-            console.log("URL:", url);
 
             fetch(url, {
                 headers: {
@@ -59,7 +60,6 @@ const SearchResults = () => {
             })
                 .then(response => response.json())
                 .then(data => {
-                    console.log("Data for workpod id", workpod, data);
                     setWorkPods((prevPods) => {
                         const newPods = [...prevPods];
 
@@ -67,10 +67,12 @@ const SearchResults = () => {
                             console.log("No reservations found for", workpodId);
                             newPods[idx].isReserved = false;
                             // calculate freefor the rest of the work day
-                            let dateEnd = setHours(date, 20);
+                            let dateEnd = date;
+                            dateEnd = setHours(dateEnd, 20);
                             dateEnd = setMinutes(dateEnd, 0);
                             newPods[idx].freeFor = differenceInMinutes(dateEnd, date);
-                            newPods[idx].events = [];
+                            newPods[idx] = { ...newPods[idx], events: data };
+
                             return newPods;
                         }
                         console.log("Reservations found for", workpodId, data);
@@ -78,14 +80,14 @@ const SearchResults = () => {
                         const isReserved = data.some((event: { start: string, end: string }) => {
                             const startDate = new Date(event.start);
                             const endDate = new Date(event.end);
-                            return isWithinInterval(date, { start: startDate, end: endDate });
+                            return isWithinInterval(dateMinute, { start: startDate, end: endDate });
                         });
                         let freeFor = 0;
                         //if isReserved is false, calculate freeFor
                         if (!isReserved) {
-                            const sortedEvents = [...data].sort((a, b) => new Date(a.start).getTime() - new Date(b.start).getTime());
+                            //const sortedEvents = [...data].sort((a, b) => new Date(b.start).getTime() - new Date(a.start).getTime());
                             //check for the next reservation
-                            const nextReservation = sortedEvents.find((event: { start: string }) => {
+                            const nextReservation = data.find((event: { start: string }) => {
                                 const startDate = new Date(event.start);
                                 return startDate > date;
                             });
@@ -96,17 +98,26 @@ const SearchResults = () => {
 
                             } else {
                                 // no reservations after the selected date, free for the rest of the day
-                                let dateEnd = setHours(date, 20);
+                                let dateEnd = date;
+                                dateEnd = setHours(dateEnd, 20);
                                 dateEnd = setMinutes(dateEnd, 0);
                                 freeFor = differenceInMinutes(dateEnd, date);
                             }
                         }
-                        newPods[idx] = { ...newPods[idx], isReserved, freeFor, events: data };
+                        newPods[idx] = { idx, isReserved, freeFor, events: data };
                         return newPods;
                     });
+                    console.log("Data for workpod id", workpod, data);
+
                 })
                 .catch(error => console.error(error));
 
+            workPods.filter((workpod) => {
+                return !workpod.isReserved;
+            });
+            workPods.sort((a, b) => {
+                return b.freeFor - a.freeFor;
+            });
             setLoading(false);
         });
     }, [workPods.length, date]);
@@ -128,9 +139,7 @@ const SearchResults = () => {
                     // need to remove any reserved workpods from the list
                     // we also need to sort workpods by freeFor
                     // so we can show the one with most time available first
-                    workPods.sort((a, b) => {
-                        return b.freeFor - a.freeFor;
-                    }).filter((workpod) => !workpod.isReserved).map((workpod, idx) => {
+                    workPods.map((workpod, idx) => {
                         let minutes = workpod.freeFor;
                         let hours = minutesToHours(minutes);
                         let minutesLeft = minutes % 60;
