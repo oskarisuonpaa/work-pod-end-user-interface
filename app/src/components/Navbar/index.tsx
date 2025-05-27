e750b154cd4041e768ae817d715948605be359af
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-05-27 14:27:42 +0300

    renamed files

---
+++ app/src/components/Navbar/index.tsx
@@ -0,0 +1,69 @@
+import { useTranslation } from "react-i18next";
+import { Link, useLocation } from "react-router";
+import { useAuth } from "@auth/useAuth";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const { isAuthenticated, onLogout } = useAuth();
+  const location = useLocation();
+  const loggedIn = isAuthenticated();
+
+  const { t, i18n } = useTranslation();
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  const isActive = (path: string) => location.pathname === path;
+
+  const NavLink = ({ to, label }: { to: string; label: string }) => (
+    <Link to={to} className={isActive(to) ? "active-link" : ""}>
+      {label}
+    </Link>
+  );
+
+  return (
+    <>
+      <nav className="navbar">
+        {loggedIn && (
+          <>
+            <NavLink to="/dashboard" label={t("navbar-dashboard")} />
+            <NavLink to="/workpods" label={t("navbar-workpods")} />
+            <NavLink to="/reservations" label={t("navbar-reservations")} />
+            <NavLink to="/search" label={t("navbar-search")} />
+          </>
+        )}
+        <NavLink to="/info" label="Info" />
+
+        {!loggedIn && <NavLink to="/login" label={t("navbar-login")} />}
+
+        {loggedIn && (
+          <Link
+            to="/login"
+            onClick={(e) => {
+              e.preventDefault();
+              onLogout();
+            }}
+          >
+            {t("navbar-logout")}
+          </Link>
+        )}
+        <span className="language-selector">
+          <button
+            className={i18n.language === "en" ? "lng-active" : "lng"}
+            onClick={() => changeLanguage("en")}
+          >
+            EN
+          </button>
+          <button
+            className={i18n.language === "fi" ? "lng-active" : "lng"}
+            onClick={() => changeLanguage("fi")}
+          >
+            FI
+          </button>
+        </span>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ app/src/components/Workpod/index.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router";
+import { useAuth } from "@auth/useAuth";
+import useWorkpodCalendar from "@hooks/useWorkpodCalendar";
+import {
+  postReservation,
+  deleteReservation,
+} from "@utils/backendCommunication";
+
+import "./Workpod.css";
+import PageWrapper from "../PageWrapper";
+import CancelButton from "./CancelButton";
+import ReserveButton from "./ReserveButton";
+import WorkpodCalendar from "./WorkpodCalendar";
+
+const Workpod = () => {
+  const { user } = useAuth();
+  const { workpodId } = useParams<{ workpodId: string }>();
+  const { events, setEvents } = useWorkpodCalendar(workpodId);
+  const navigate = useNavigate();
+
+  const [selectedSlot, setSelectedSlot] = useState<{
+    start: string;
+    end: string;
+    status: string;
+    title: string;
+    eventId?: string;
+  } | null>(null);
+
+  const handleReservation = async (slot: { start: string; end: string }) => {
+    if (!workpodId || !user?.name) return;
+
+    if (confirm("Are you sure you want to reserve this slot?")) {
+      await postReservation(workpodId, slot.start, slot.end);
+      navigate(0);
+    }
+  };
+
+  const handleCancelReservation = async (slot: {
+    start: string;
+    end: string;
+    eventId?: string;
+  }) => {
+    if (!workpodId || !slot.eventId) return;
+
+    if (confirm("Are you sure you want to cancel this reservation?")) {
+      await deleteReservation(workpodId, slot.eventId);
+
+      const updatedEvents = events.filter((event) => event.id !== slot.eventId);
+
+      const freeSlot = {
+        id: `${slot.start}-${slot.end}-free`,
+        title: "Free",
+        start: new Date(slot.start),
+        end: new Date(slot.end),
+      };
+
+      setEvents([...updatedEvents, freeSlot]);
+      setSelectedSlot(null);
+    }
+  };
+
+  if (!workpodId) return <div>Workpod ID is missing</div>;
+
+  return (
+    <PageWrapper pageTitle={workpodId}>
+      <WorkpodCalendar events={events} onSlotSelect={setSelectedSlot} />
+      {selectedSlot && selectedSlot.status === "free" && (
+        <ReserveButton slot={selectedSlot} onReserve={handleReservation} />
+      )}
+      {selectedSlot && selectedSlot.title === user?.name && (
+        <CancelButton slot={selectedSlot} onCancel={handleCancelReservation} />
+      )}
+    </PageWrapper>
+  );
+};
+
+export default Workpod;
