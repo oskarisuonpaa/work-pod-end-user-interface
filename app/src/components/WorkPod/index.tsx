42a6c8e10022fc7173e3fbc92a355c4f7fd3d49a
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-05-27 10:18:16 +0300

    Refactor Workpod component: streamline reservation logic by removing unnecessary state updates and enhancing cancellation handling.

--- app/src/components/WorkPod/index.tsx
+++ app/src/components/WorkPod/index.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useState } from "react";
 import { useAuth } from "../../auth/useAuth";
 import WorkpodCalendar from "./WorkpodCalendar";
@@ -18,6 +18,7 @@ const Workpod = () => {
   const { user } = useAuth();
   const { workpodId } = useParams<{ workpodId: string }>();
   const { events, setEvents } = useWorkpodCalendar(workpodId);
+  const navigate = useNavigate();
 
   const [selectedSlot, setSelectedSlot] = useState<{
     start: string;
@@ -32,23 +33,7 @@ const Workpod = () => {
 
     if (confirm("Are you sure you want to reserve this slot?")) {
       await postReservation(workpodId, slot.start, slot.end);
-
-      const updatedEvents = events.filter(
-        (event) =>
-          !(isSameSlot(event, slot) && event.extendedProps?.status === "free")
-      );
-
-      const reservedSlot = {
-        title: user.name,
-        start: slot.start,
-        end: slot.end,
-        backgroundColor: "#3b82f6",
-        borderColor: "#3b82f6",
-        textColor: "#fff",
-        extendedProps: { status: "reserved" },
-      };
-
-      setEvents([...updatedEvents, reservedSlot]);
+      navigate(0);
     }
   };
 
@@ -57,21 +42,30 @@ const Workpod = () => {
     end: string;
     eventId?: string;
   }) => {
-    console.log("handleCancelReservation", slot);
     if (!workpodId || !slot.eventId) return;
 
     if (confirm("Are you sure you want to cancel this reservation?")) {
       await deleteReservation(workpodId, slot.eventId);
 
       const updatedEvents = events.filter((event) => event.id !== slot.eventId);
-      setEvents(updatedEvents);
+
+      const freeSlot = {
+        title: "Free",
+        start: slot.start,
+        end: slot.end,
+        backgroundColor: "var(--green)",
+        borderColor: "#c3e6cb",
+        extendedProps: {
+          status: "free",
+        },
+      };
+
+      setEvents([...updatedEvents, freeSlot]);
       setSelectedSlot(null);
     }
   };
 
   if (!workpodId) return <div>Workpod ID is missing</div>;
-
-  console.log(selectedSlot);
 
   return (
     <PageWrapper pageTitle={workpodId}>
