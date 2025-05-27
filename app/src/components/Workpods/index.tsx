85b5b1ad07e7d4764029d88723de1b26fd05d594
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-05-27 14:37:15 +0300

    fix: correct import path for Workpods.css

--- app/src/components/Workpods/index.tsx
+++ app/src/components/Workpods/index.tsx
@@ -1,4 +1,4 @@
-import "./WorkPods.css";
+import "./Workpods.css";
 import { Fragment, useEffect, useState } from "react";
 import { getWorkpods } from "@utils/backendCommunication";
 import PageWrapper from "../PageWrapper";
