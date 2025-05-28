ce20fcefe474faa142a78410e78cf8485a158066
eleino <leinonemmi@gmail.com>
2025-05-28 13:56:58 +0300

    Added i18n for Info page

--- app/src/components/InfoPage/index.tsx
+++ app/src/components/InfoPage/index.tsx
@@ -2,32 +2,29 @@ import { useAuth } from "@auth/useAuth.ts";
 import PageWrapper from "../PageWrapper";
 import "./Info.css";
 import ActionButton from "@components/ActionButton";
+import { useTranslation } from "react-i18next";
 
 const InfoPage = () => {
   const { isAuthenticated } = useAuth();
   const loggedIn = isAuthenticated();
+  const {t} = useTranslation();
 
   return (
-    <PageWrapper pageTitle="Info - Workpod Reservation System">
+    <PageWrapper pageTitle={"Info - "+t("info-title")}>
       {!loggedIn && (
         <p>
-          Note: access is for authorized users only; log in using LAB/LUT
-          credentials.
+          {t("info-access-text")}
           <br />
-          <ActionButton label="Login" to="/login" />
+          <ActionButton label={t("navbar-login")} to="/login" />
         </p>
       )}
 
       <p>
-        To reserve a work pod, select a work pod from the workpods page, or
-        search for an available work pod using the search page. You can view
-        your reservations on the dashboard page.
+        {t("info-text1")}
       </p>
 
       <p>
-        To cancel a reservation, go to the dashboard page and select the
-        reservation you want to cancel. Please cancel your reservation if you no
-        longer need it to make it available for another user.
+        {t("info-text2")}
       </p>
     </PageWrapper>
   );
