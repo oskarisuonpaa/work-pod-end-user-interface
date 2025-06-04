38d08a6f75e54d29863128c690038cedbd610cda
eleino <emilia.leino@proton.me>
2025-06-04 13:38:27 +0300

    Converted table to css grid and added accessibility to search page

--- app/src/components/Search/index.tsx
+++ app/src/components/Search/index.tsx
@@ -38,47 +38,35 @@ const Search = () => {
   return (
     <PageWrapper pageTitle={t("search-title")}>
       <p>{t("search-reservation-info")}</p>
-      <form id="searchForm" onSubmit={handleSubmit}>
-        <table>
-          <tbody>
-            <tr>
-              <td>
-                <label htmlFor="date">{t("date")}:</label>
-              </td>
-              <td>
-                <DatePicker
-                  selected={startDate}
-                  onChange={(date) => setStartDate(date)}
-                  includeDateIntervals={[
-                    { start: new Date(), end: addDays(new Date(), 30) },
-                  ]}
-                  filterDate={isWeekday}
-                  dateFormat="dd/MM/yyyy"
-                  selectsDisabledDaysInRange
-                  calendarClassName="custom-calendar"
-                  minDate={new Date()}
-                  maxDate={addDays(new Date(), 30)}
-                />
-              </td>
-            </tr>
-            <tr>
-              <td>
-                <label htmlFor="time">{t("search-label-time")}:</label>
-              </td>
-              <td>
-                <DatePicker
-                  showTimeSelect
-                  selected={startDate}
-                  onChange={(time) => setStartDate(time)}
-                  showTimeSelectOnly
-                  dateFormat="HH:mm"
-                  calendarClassName="custom-time"
-                />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <ActionButton label={t("search-button")} type="submit" />
+      <form id="searchForm" onSubmit={handleSubmit} className="search-form">
+          <label htmlFor="date">{t("date")}:</label>
+
+          <DatePicker
+          id="date"
+            selected={startDate}
+            onChange={(date) => setStartDate(date)}
+            includeDateIntervals={[
+              { start: new Date(), end: addDays(new Date(), 30) },
+            ]}
+            filterDate={isWeekday}
+            dateFormat="dd/MM/yyyy"
+            selectsDisabledDaysInRange
+            calendarClassName="custom-calendar"
+            minDate={new Date()}
+            maxDate={addDays(new Date(), 30)}
+          />
+          <label htmlFor="time">{t("search-label-time")}:</label>
+
+          <DatePicker
+            id="time"
+            showTimeSelect
+            selected={startDate}
+            onChange={(time) => setStartDate(time)}
+            showTimeSelectOnly
+            dateFormat="HH:mm"
+            calendarClassName="custom-time"
+          />
+          <ActionButton label={t("search-button")} type="submit" className="last-row" />
       </form>
     </PageWrapper>
   );
