70f347c1dc0a8a88a6edd0c260a2b65e728025e1
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-06-19 09:13:38 +0300

    Refactor ScanPage to handle QR code scanning and navigation

--- app/src/components/ScanPage.tsx
+++ app/src/components/ScanPage.tsx
@@ -1,14 +1,24 @@
+import { useNavigate } from "react-router";
 import PageWrapper from "./PageWrapper";
 import { Scanner } from "@yudiel/react-qr-scanner";
 
+type ScanData = {
+  rawValue: string;
+};
+
 const ScanPage = () => {
+  const navigate = useNavigate();
+
+  const handleScan = (data: ScanData[] | null) => {
+    if (!data) return;
+
+    const scannedURL = data[0].rawValue;
+    navigate(scannedURL, { replace: true });
+  };
+
   return (
     <PageWrapper pageTitle="Scan QR Code">
-      <Scanner
-        onScan={(data) => {
-          console.log("Scanned data:", data);
-        }}
-      />
+      <Scanner onScan={handleScan} />
     </PageWrapper>
   );
 };
