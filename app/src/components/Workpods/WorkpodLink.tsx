1c8bd133558eab3b4a7635d1076a92c0ef3c3261
Oskari Suonpää <oskarisuonpaa@outlook.com>
2025-06-25 09:35:03 +0300

    rip peepo

--- app/src/components/Workpods/WorkpodLink.tsx
+++ app/src/components/Workpods/WorkpodLink.tsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router";
-import peepo from "../../assets/placeholder-pepe.jpg";
+import koppero from "../../assets/koppero.png";
 import type { Workpod } from "@types";
 
 const WorkpodLink = ({ alias, status }: Workpod) => {
   return (
     <Link to={"/workpods/" + alias} className="work-pod-link">
       <div className={`hexagon ${status}`}>
-        <img src={peepo} alt="Peepo" />
+        <img src={koppero} alt="Peepo" />
         <p className="work-pod-name">{alias}</p>
       </div>
     </Link>
