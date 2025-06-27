a92b4b3561237c5599f0f963371da61ecb722ca2
eleino <emilia.leino@proton.me>
2025-06-27 10:29:12 +0300

    Added availability info to workpodlink

--- app/src/components/Workpods/WorkpodLink.tsx
+++ app/src/components/Workpods/WorkpodLink.tsx
@@ -1,14 +1,39 @@
 import { Link } from "react-router";
 import koppero from "../../assets/koppero.png";
 import type { Workpod } from "@types";
+import { useEffect, useState } from "react";
+import { getWorkpodCalendar } from "api/workpods";
+import { getWorkpodAvailability } from "../SearchResults/getWorkpodAvailability";
+import type { WorkpodAvailability } from "../SearchResults/getWorkpodAvailability";
+
+
 
 const WorkpodLink = ({ alias, status }: Workpod) => {
+    const [availability, setAvailability] = useState<WorkpodAvailability | null>(null);
+
+  useEffect(() => {
+    const fetchAvailability = async () => {
+      const now = new Date();
+      const endOfDay = new Date(now);
+      endOfDay.setHours(23, 59, 59);
+      const events = await getWorkpodCalendar(alias, now.toISOString(), endOfDay.toISOString());
+      const avail = getWorkpodAvailability(events, now);
+      setAvailability(avail);
+    };
+    fetchAvailability();
+  }, [alias]);
   return (
     <Link to={"/workpods/" + alias} className="work-pod-link">
       <div className={`hexagon ${status}`}>
         <p className="work-pod-name">{alias}</p>
         <img src={koppero} alt="Workpod" />
-        <p className="availability-timeframe"> 24H</p>
+        <p className="availability-timeframe">
+          {availability
+            ? availability.isReserved
+              ? `${Math.floor(availability.reservedFor / 60)}h ${availability.reservedFor % 60}min`
+              : `${Math.floor(availability.freeFor / 60)}h ${availability.freeFor % 60}min`
+            : ""}
+        </p>
       </div>
     </Link>
   );
