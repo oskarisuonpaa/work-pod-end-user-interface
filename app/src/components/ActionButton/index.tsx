41bdaddb42cc8a2cfc39a6d658c5f733a4cba8fa
Emilia Leino <emilia.leino@proton.me>
2025-07-01 12:00:20 +0300

    Button docs

--- app/src/components/ActionButton/index.tsx
+++ app/src/components/ActionButton/index.tsx
@@ -23,6 +23,11 @@ type ActionButtonProps = CommonProps & (LinkVariant | ButtonVariant);
  * ActionButton component renders a button or a link with a label.
  * It can be used for navigation or actions within the application.
  * @module ActionButton
+ * @props {ActionButtonProps}
+ * @param {string} label - The text to display on the button or link.
+ * @param {string} [className] - Additional CSS classes to apply to the button or link.
+ * @param {string} [to] - The route to navigate to when the button is clicked (if using as a link).
+ * @param {React.ButtonHTMLAttributes<HTMLButtonElement>} [rest] - Additional attributes for the button element (if using as a button).
  * @description This component is used to create buttons that can either navigate to a different route or perform an action.
  */
 const ActionButton = forwardRef<HTMLButtonElement, ActionButtonProps>(
